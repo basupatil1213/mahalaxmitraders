021ab18698b53b9d46d12e7d7abfaaace1e7d24f
Basavaraj Patil <patil.basava@outlook.com>
2023-12-24 23:07:53 -0500

    completed landing page with device compatibility

--- mahalaxmitraders-frontend/src/components/Home/Home.jsx
+++ mahalaxmitraders-frontend/src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import heroImg from './resources/images/hero-image.png';
+import ownerImg from './resources/images/owner-image.png'
 import './Home.styles.css'
+
 
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 const Category = ({ category }) => {
     return (
-        <div className="category-card mb-10 mx-5 h-96 flex  flex-col gap-5 items-center justify-center px-5 shadow-2xl">
+        <div className="category-card mb-10 mx-5 h-96 flex w-[85%] md:w-auto flex-col gap-5 items-center justify-center px-5 shadow-2xl">
             <img src={category.image} alt={category.name} className="product--image" />
             <h1 className="">{category.name}</h1>
             <a href="/search" className="py-3 px-5 bg-tertiary rounded-md hover:text-secondary hover:bg-primary">See More</a>
@@ -16,23 +18,23 @@ const Category = ({ category }) => {
 }
 
 const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
 };
 
 // sampleData
@@ -66,6 +68,34 @@ const Home = () => {
 
     const [categories, setCategories] = React.useState(sampleData);
 
+    const [deviceType, setDeviceType] = useState('desktop');
+
+    useEffect(() => {
+        // Update device type on window resize
+        const handleResize = () => {
+            setDeviceType(getDeviceType());
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [deviceType]);
+
+    // Function to determine device type based on screen width
+    const getDeviceType = () => {
+        const screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            return 'mobile';
+        } else if (screenWidth < 1024) {
+            return 'tablet';
+        } else {
+            return 'desktop';
+        }
+    }
+
     React.useEffect(() => {
         // fetch('http://localhost:5000/api/categories')
         //     .then(res => res.json())
@@ -75,8 +105,8 @@ const Home = () => {
         //     .catch(err => console.log(err));
         setCategories(sampleData);
     }
-    , []);
-    
+        , []);
+
     return (
         <>
             {/*  Hero Section   */}
@@ -90,12 +120,12 @@ const Home = () => {
                         <a href="/search" className="hero-section__content__cta__link bg-tertiary p-4 rounded-md text-secondary font-extrabold hover:bg-primary">Search Now</a>
                     </div>
                 </div>
-                <div className="hero-section__image self-center hidden lg:flex">
+                <div className="hero-section__image self-center hidden lg:flex shadow-2xl">
                     <img src={heroImg} alt="Hero" className="h-screen-70" />
                 </div>
             </div>
             {/* Category Section */}
-            <div className="flex flex-col gap-5 px-10 my-10 md:mt-0">
+            <div className="flex flex-col gap-5 px-0 md:px-10 my-10 md:mt-0">
                 <div className="text-center flex flex-col gap-8">
                     <p className=" text-3xl font-italic-ibm">Quality and Style are to tiles what water and sunshine are to plants</p>
                     <h1 className="text-4xl font-primary font-bold">Categories</h1>
@@ -106,20 +136,21 @@ const Home = () => {
                         showDots={true}
                         responsive={responsive}
                         ssr={true} // means to render carousel on server-side.
-                        infinite={false}
-                        autoPlay={false}
-                        autoPlaySpeed={1000}
+                        infinite={true}
+                        autoPlay={deviceType === "mobile" ? true : false}
+                        autoPlaySpeed={2000}
                         keyBoardControl={true}
                         containerClass="carousel-container"
                         removeArrowOnDeviceType={["tablet", "mobile"]}
-                        deviceType={"desktop"}
+                        deviceType={deviceType}
                         dotListClass="custom-dot-list-style"
                         itemClass=""
-                        className="" >
+                        preventMovementUntilSwipeScrollTolerance={true}
+                        className="w-full md:w-auto" >
                         {
                             categories.map((category, index) => (
-                                 <Category key={index} category={category} />
-                                
+                                <Category key={index} category={category} />
+
                             ))
                         }
                     </Carousel>
@@ -132,11 +163,11 @@ const Home = () => {
                 </div>
                 <div className="flex flex-col-reverse lg:flex-row justify-around items-center sm:gap-0 lg:gap-20 h-[80vh] mx-20 mb-10 md:mb-20">
                     <div className="owner-detail flex flex-col">
-                        <h1 className="sm:text-9xl md:text-7xl lg:text-9xl font-primary font-bold">Arun <br />Muddennavar</h1>
+                        <h1 className="sm:text-5xl md:text-7xl lg:text-9xl font-primary font-bold">Arun <br />Muddennavar</h1>
                         <p className="text-2xl font-tertiary">Passionate owner dedicated to providing a curated selection of high-quality and stylish tiles for every customer's unique needs.</p>
                     </div>
                     <div className="owner-image shadow-2xl">
-                        <img src="https://media.istockphoto.com/photos/portrait-of-handsome-smiling-young-man-with-crossed-arms-picture-id1200677760?k=6&m=1200677760&s=612x612&w=0&h=IXURU5rnCRyN5h5ebeDLQcVv0lNs0ZLgE98fPioSMnQ=" alt="Owner" className="" />
+                        <img src={ownerImg} alt="Owner" className="" />
                     </div>
                 </div>
             </section>
